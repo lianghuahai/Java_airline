a0b991b830ecb8b6b1b65b157dda0494757aef86
Limeng <limeng.ruan@stonybrook.edu>
2017-12-05 21:32:53 -0500

    Added 3.1 4,5

--- src/Bean/Reservation.java
+++ src/Bean/Reservation.java
@@ -1,9 +1,20 @@
 package Bean;
 
 public class Reservation {
+	private int reservationNo;
 	private String ReservationDate;
 	private double bookingFee;
 	private double totalFare;
+	
+	private int flightNo;
+	private String airlineName;
+	private int accountNo;
+	
+	private String firstName;
+	private String lastName;
+	
+	private String destinationCity;
+	
 	public String getReservationDate() {
 		return ReservationDate;
 	}
@@ -21,5 +32,47 @@ public class Reservation {
 	}
 	public void setTotalFare(double totalFare) {
 		this.totalFare = totalFare;
+	}
+	public int getReservationNo() {
+		return reservationNo;
+	}
+	public void setReservationNo(int reservationNo) {
+		this.reservationNo = reservationNo;
+	}
+	public int getFlightNo() {
+		return flightNo;
+	}
+	public void setFlightNo(int flightNo) {
+		this.flightNo = flightNo;
+	}
+	public String getAirlineName() {
+		return airlineName;
+	}
+	public void setAirlineName(String airlineName) {
+		this.airlineName = airlineName;
+	}
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDestinationCity() {
+		return destinationCity;
+	}
+	public void setDestinationCity(String destinationCity) {
+		this.destinationCity = destinationCity;
 	}
 }
--- src/Service/ReservationService.java
+++ src/Service/ReservationService.java
@@ -12,5 +12,17 @@ public class ReservationService {
 	public List<Reservation> getMonthReport(String dateFrom, String dateTo){
 		return rd.getMonthReport(dateFrom, dateTo);
 	}
+	
+	public List<Reservation> getReservationsByFlight(String airlineName, int flightNo){
+		return rd.getReservationsByFlight(airlineName, flightNo);
+	}
+	
+	public List<Reservation> getReservationsByCustomer(String firstName, String lastName){
+		return rd.getReservationsByCustomer(firstName, lastName);
+	}
+	
+	public List<Reservation> getReservationsByDestinationCity(String destinationCity){
+		return rd.getReservationsByDestinationCity(destinationCity);
+	}
 
 }
